f11368beea45ef0e5f03fd51f34921dd95c297c8
Mokhtar Zehrawi <mokhtar.zehrawi@gmail.com>
2021-03-30 17:48:33 -0400

    added branch hazard detection to hazard detection unit

--- hazard_detection_unit.vhd
+++ hazard_detection_unit.vhd
@@ -1,7 +1,8 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
---1. If (ID/EX.MemRead & ((ID/EX.RegisterRt = IF/ID.RegisterRs) or (ID/EX.RegisterRt = IF/ID.RegisterRt)))
+--1. If (ID/EX.MemRead & ((ID/EX.RegisterRt = IF/ID.RegisterRs) or (ID/EX.RegisterRt = IF/ID.RegisterRt))) <-- lw stall (hazard detected)
+--2  If (IF/ID.op = 000100 AND data1 == data2) <--branch stall
 
 ENTITY hazard_detection_unit IS
 	PORT (
@@ -9,14 +10,17 @@ ENTITY hazard_detection_unit IS
                 in_ID_EX_rt		  : IN STD_LOGIC_VECTOR (2 downto 0);
 		in_IF_ID_rs, in_IF_ID_rt  : IN STD_LOGIC_VECTOR (2 downto 0);
 
+		in_IF_ID_op		  : IN STD_LOGIC_VECTOR (5 downto 0);
+		in_Data1_eq_Data2	  : IN STD_LOGIC;
 		
+
 		o_controlMux, o_IF_ID_write, o_PC_write    : OUT STD_LOGIC );
 END;
 
 ARCHITECTURE struct OF hazard_detection_unit IS
 
 	SIGNAL int_ID_EX_rt_eq_IF_ID_rs, int_ID_EX_rt_eq_IF_ID_rt    : STD_LOGIC;
-	SIGNAL int_hazard					     : STD_LOGIC;
+	SIGNAL int_hazard, int_branch_stall, int_branch_if_eq	     : STD_LOGIC;
 
 BEGIN
 	--Signal Assignment
@@ -27,11 +31,13 @@ BEGIN
 	
 	int_hazard <= in_ID_EX_MemRead AND (int_ID_EX_rt_eq_IF_ID_rs OR int_ID_EX_rt_eq_IF_ID_rt);
 
+	int_branch_if_eq <= (not(in_IF_ID_op(5)) AND not(in_IF_ID_op(4)) AND not(in_IF_ID_op(3)) AND in_IF_ID_op(2) AND not(in_IF_ID_op(1)) AND not(in_IF_ID_op(0)) );
+	int_branch_stall <= int_branch_if_eq AND in_Data1_eq_Data2;
 
 	--Output Assignment
 	o_controlMux 	<= int_hazard;
 	o_PC_write	<= not(int_hazard);
-	o_IF_ID_write	<= not(int_hazard);
+	o_IF_ID_write	<= not(int_hazard or int_branch_stall);
 	
 
 END struct;
