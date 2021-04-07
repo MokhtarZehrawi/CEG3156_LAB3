96052af796eec7b92674198e23c11084a9492911
Mokhtar Zehrawi <mokhtar.zehrawi@gmail.com>
2021-04-06 20:39:37 -0400

    another forward unit update

--- forward_unit.vhd
+++ forward_unit.vhd
@@ -1,10 +1,15 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
---1. If (EX/MEM.RegWrite & (EX/MEM.RegisterRd != 0) & (EX/MEM.RegisterRd=ID/EX.RegisterRs)) ForwardA= 10 <-- (type 1 r-type  hazard)
---2. If (EX/MEM.RegWrite & (EX/MEM.RegisterRd != 0) & (EX/MEM.RegisterRd=ID/EX.RegisterRt)) ForwardB= 10 <-- (type 2 r-type  hazard)
---3. If (MEM/WB.RegWrite & (MEM/WB.RegisterRd != 0) & (MEM/WB.RegisterRd=ID/EX.RegisterRs)) ForwardA= 01 <-- (type 3 r-type  hazard)
---4. If (MEM/WB.RegWrite & (MEM/WB.RegisterRd != 0) & (MEM/WB.RegisterRd=ID/EX.RegisterRt)) ForwardB= 01 <-- (type 4 r-type  hazard)
+--1. If (EX/MEM.RegWrite & (EX/MEM.RegisterRd != 0) & (EX/MEM.RegisterRd = ID/EX.RegisterRs)) ForwardA= 10 <-- (type 1 r-type  hazard)
+--2. If (EX/MEM.RegWrite & (EX/MEM.RegisterRd != 0) & (EX/MEM.RegisterRd = ID/EX.RegisterRt)) ForwardB= 10 <-- (type 2 r-type  hazard)
+--3. If (MEM/WB.RegWrite & (MEM/WB.RegisterRd != 0) & (MEM/WB.RegisterRd = ID/EX.RegisterRs)) ForwardA= 01 <-- (type 3 r-type  hazard)
+--4. If (MEM/WB.RegWrite & (MEM/WB.RegisterRd != 0) & (MEM/WB.RegisterRd = ID/EX.RegisterRt)) ForwardB= 01 <-- (type 4 r-type  hazard)
+
+--5. If (EX/MEM.RegWrite & (EX/MEM.RegisterRd != 0) & ID/EX.MemWrite & (EX/MEM.RegisterRd = ID/EX.RegisterRt)) <-- (type1 sw hazard)
+--6. If (EX/MEM.RegWrite & (EX/MEM.RegisterRd != 0) & ID/EX.MemWrite & (EX/MEM.RegisterRd = ID/EX.RegisterRs)) <-- (type2 sw hazard) (not implemented here)
+
+
 
 ENTITY forward_unit IS
 	PORT (
@@ -14,8 +19,10 @@ ENTITY forward_unit IS
 		
 		in_EX_MEM_RegWrite        : IN STD_LOGIC;
 		in_MEM_WB_RegWrite        : IN STD_LOGIC;
+		in_ID_EX_MemWrite         : IN STD_LOGIC;
 
-		o_forwardA, o_forwardB    : OUT STD_LOGIC_VECTOR (1 downto 0) );
+		o_forwardA, o_forwardB    : OUT STD_LOGIC_VECTOR (1 downto 0);
+		o_forwardC		  : OUT STD_LOGIC );
 END;
 
 ARCHITECTURE struct OF forward_unit IS
@@ -23,7 +30,8 @@ ARCHITECTURE struct OF forward_unit IS
 	SIGNAL int_EX_MEM_rd_notZero, int_MEM_WB_rd_notZero   		  : STD_LOGIC;
 	SIGNAL int_EX_MEM_rd_eq_ID_EX_rs, int_EX_MEM_rd_eq_ID_EX_rt    	  : STD_LOGIC;
 	SIGNAL int_MEM_WB_rd_eq_ID_EX_rs, int_MEM_WB_rd_eq_ID_EX_rt    	  : STD_LOGIC;
-	SIGNAL int_type1, int_type2, int_type3, int_type4		  : STD_LOGIC;
+	SIGNAL int_rtype1, int_rtype2, int_rtype3, int_rtype4		  : STD_LOGIC;
+	SIGNAL int_swtype1, int_swtype2					  : STD_LOGIC;
 	
 BEGIN
 	--Signal Assignment
@@ -35,20 +43,23 @@ BEGIN
 	int_MEM_WB_rd_eq_ID_EX_rs <= (in_MEM_WB_rd(0) XNOR in_ID_EX_rs(0)) AND (in_MEM_WB_rd(1) XNOR in_ID_EX_rs(1)) AND (in_MEM_WB_rd(2) XNOR in_ID_EX_rs(2));
 	int_MEM_WB_rd_eq_ID_EX_rt <= (in_MEM_WB_rd(0) XNOR in_ID_EX_rt(0)) AND (in_MEM_WB_rd(1) XNOR in_ID_EX_rt(1)) AND (in_MEM_WB_rd(2) XNOR in_ID_EX_rt(2));
 
-	int_type1 <= in_EX_MEM_RegWrite AND int_EX_MEM_rd_notZero AND int_EX_MEM_rd_eq_ID_EX_rs;
-	int_type2 <= in_EX_MEM_RegWrite AND int_EX_MEM_rd_notZero AND int_EX_MEM_rd_eq_ID_EX_rt;
-	int_type3 <= in_MEM_WB_RegWrite AND int_MEM_WB_rd_notZero AND int_MEM_WB_rd_eq_ID_EX_rs;
-	int_type4 <= in_MEM_WB_RegWrite AND int_MEM_WB_rd_notZero AND int_MEM_WB_rd_eq_ID_EX_rt;
-
-
+	int_rtype1 <= in_EX_MEM_RegWrite AND int_EX_MEM_rd_notZero AND int_EX_MEM_rd_eq_ID_EX_rs;
+	int_rtype2 <= in_EX_MEM_RegWrite AND int_EX_MEM_rd_notZero AND int_EX_MEM_rd_eq_ID_EX_rt;
+	int_rtype3 <= in_MEM_WB_RegWrite AND int_MEM_WB_rd_notZero AND int_MEM_WB_rd_eq_ID_EX_rs;
+	int_rtype4 <= in_MEM_WB_RegWrite AND int_MEM_WB_rd_notZero AND int_MEM_WB_rd_eq_ID_EX_rt;
+	
+	int_swtype1 <= in_EX_MEM_RegWrite AND int_EX_MEM_rd_notZero AND in_ID_EX_MemWrite AND int_EX_MEM_rd_eq_ID_EX_rt;
+	int_swtype2 <= in_EX_MEM_RegWrite AND int_EX_MEM_rd_notZero AND in_ID_EX_MemWrite AND int_EX_MEM_rd_eq_ID_EX_rs;
+	
 
 	
 	--Output Assignment
-	o_forwardA(0) <= int_type3 AND not(int_type1);
-	o_forwardA(1) <= int_type1;
-	o_forwardB(0) <= int_type4 AND not(int_type2);
-	o_forwardB(1) <= int_type2;
+	o_forwardA(0) <= int_rtype3 AND not(int_rtype1);
+	o_forwardA(1) <= int_rtype1;
+	o_forwardB(0) <= int_rtype4 AND not(int_rtype2);
+	o_forwardB(1) <= int_rtype2;
 
+	o_forwardC    <= int_swtype1;
 
 
 
